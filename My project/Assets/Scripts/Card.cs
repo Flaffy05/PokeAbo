d5c4263db8b636ac07fb29a6dd628966d23532bf
Flaffy05 <105724877+Flaffy05@users.noreply.github.com>
2025-02-15 16:40:55 +0100

    First commit
    Uploading project files

---
+++ My project/Assets/Scripts/Card.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Pokemons new card", menuName = "Standard Card")]
+public class Card : ScriptableObject
+{
+    
+    public int cardId;
+    public string displayName = "";
+    public int hp = 0;
+    
+    public Sprite photo;
+
+    public attack[] attacchi;
+    
+    public Element cardElement;
+    //Element attackElement;
+
+}
+
+[System.Serializable]
+public class attack {
+        public string name = "";
+        public int damage = 0;
+        public Element attackElement;
+        public string description;
+        public int elementCost;
+
+}
+    
+    public enum Element {Erba, Fuoco, Acqua, Elettro, Buio, Lotta, Neutro, Metallo}; 
+public enum Status {Avvelenato, Stunnato, Stanco};  //booooooh
