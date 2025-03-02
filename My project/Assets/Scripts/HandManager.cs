af2041182eaec0edbb0b9fa3979c2afb40c26ab8
SRgreenfire <samuele.ferrari@itisondrio.org>
2025-03-02 14:45:37 +0100

    Gestione Mano

---
+++ My project/Assets/Scripts/HandManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HandManager : MonoBehaviour
+{
+    public GameObject cardPrefab; // assegna un prefab delle carte all'inspector
+    public Transform handTransform; //Root della posizione della mano
+    public float fanSpread = 5f;
+    public List<GameObject> cardsInHand= new List<GameObject>(); //tiene una lista di carte nella tua mano
+
+
+    void Start()
+    {
+      Pesca();   
+      Pesca();
+      Pesca();
+      Pesca();
+      Pesca();
+
+    }
+
+    public void Pesca(){
+       
+        GameObject newCard = Instantiate(cardPrefab, handTransform.posizione, Quaternion.identity, handTransform);
+        cardsInHand.Add(newCard);
+
+        UpdateHandVisuals();
+    }
+     
+     private void UpdateHandVisuals(){
+        int cardCount = cardsInHand.Count;
+        for(int i=0; i<cardCount; i++){
+            float rotationAngle=(fanSpread + (i-(cardCount-1)/2f));
+            cardsInHand[i].Transform.localRotation=Quaternion.Euler(0f,0f, rotationAngle);
+        }
+     }
+
+
+}
