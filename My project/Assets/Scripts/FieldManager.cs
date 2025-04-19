fe988193a80937971480dc109aee8b7f153453ad
aleabo <alessandro.abordi@gmail.com>
2025-04-19 16:07:38 +0200

    provato a fare field manager
    ho commentato non va

---
+++ My project/Assets/Scripts/FieldManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class FieldManager : MonoBehaviour
+{
+    /*public DeckManager deckManager;
+    public GameObject cardPrefab;
+    public List<Transform> fieldSlots;  // Lista degli slot nel campo
+    public List<GameObject> cardsInField = new List<GameObject>();  // Carte nel campo
+
+    void Start()
+    {
+    }
+
+    public void AddCardToField(Card cardData, Transform targetSlot)
+    {
+        GameObject newCard = Instantiate(cardPrefab, targetSlot.position, Quaternion.identity, targetSlot);
+        cardsInField.Add(newCard);
+        newCard.GetComponent<CardDisplay>().cardData = cardData;
+    }
+
+    public void RemoveCardFromField(GameObject card)
+    {
+        if (cardsInField.Contains(card))
+        {
+            cardsInField.Remove(card);
+            Destroy(card);
+        }
+    }
+
+    void Update()
+    {
+    }*/
+}
+
+
+
